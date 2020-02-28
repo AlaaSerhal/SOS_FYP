c43eca8dec3fca94701a100ee2c0ea7426ccafb6
Alaa_Serhal <alaa.serhal@outlook.com>
2020-02-28 13:02:55 +0200

    Compatible with RPI

--- FIre_Detection_only/convolutional_fire_detection.py
+++ FIre_Detection_only/convolutional_fire_detection.py
@@ -1,31 +1,40 @@
 #import the necessary libraries
 import tensorflow as tf
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing.image import img_to_array
-from picamera.array import PiRGBArray
-from picamera import PiCamera
 import numpy as np
 import cv2
 import time
+import keras
 
+from keras.models import load_model
+
+from keras.utils import CustomObjectScope
+
+from keras.preprocessing.image import img_to_array
+
+from keras.initializers import glorot_uniform
+
+with CustomObjectScope({'GlorotUniform': glorot_uniform()}):
+
+        model = load_model('/home/pi/SOS_FYP/FIre_Detection_only/Fire-64x64-color-v7-soft.h5')
 # loading the stored model from file
-model=load_model('/home/alaa/Desktop/SOS_FYP/FIre_Detection_only/Fire-64x64-color-v7-soft.h5')
+#model=load_model('/home/pi/SOS_FYP/FIre_Detection_only/Fire-64x64-color-v7-soft.h5')
 #capture frames from the camera with source ID 0
-#cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0)
 #give the camera time to heat up
 time.sleep(2)
-camera = PiCamera()
-camera.resolution = (640, 480)
-camera.framerate = 32
-rawCapture = PiRGBArray(camera, size=(640, 480))
-# allow the camera to warmup
-time.sleep(0.1)
-# capture frames from the camera
-for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-	# grab the raw NumPy array representing the image, then initialize the timestamp
-	# and occupied/unoccupied text
-	image = frame.array
-	orig=image.copy()
+#if there is a frame
+if cap.isOpened(): # try to get the first frame
+    rval, frame = cap.read()
+else:
+    rval = False
+IMG_SIZE = 64
+
+while(1):
+    #keep on reading the frames received
+    rval, image = cap.read()
+    if rval==True:
+        #image will be used to be shown on the lvie feed
+        orig= image.copy()
         #resize image for better processing speeds
         image = cv2.resize(image, (IMG_SIZE, IMG_SIZE))  
         image = image.astype("float") / 255.0
